f297a6cac94c0892e9634582a83839aadd51aae2
pezzaliapp <pezzalialessandro@gmail.com>
2025-08-27 15:26:31 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
-const CACHE = 'allineamenti-v1';
+// cambia la versione per forzare l'update della cache
+const CACHE = 'allineamenti-v2';   // <--- incrementa (v3, v4, ...)
+
 const ASSETS = [
   './',
   './index.html',
@@ -8,13 +10,22 @@ const ASSETS = [
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting()) // attiva subito il nuovo SW
+  );
 });
+
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim()) // prendi controllo subito
+  );
 });
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then(resp => resp || fetch(e.request).then(r => {
